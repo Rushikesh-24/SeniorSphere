ff0d446bbf75e5a885a386e1dec3fe740f0baff8
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2023-11-10 17:21:44 +0530

    updated footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,10 +6,9 @@ const Footer = () => {
         <div id="footer" className='flex flex-col justify-center items-center gap-10 mt-20'>
         <div id="footerlogo" className='flex items-center gap-5'>
             <img src="/Assets/logo.svg" alt="" className='w-40'/>
-            <p className='text-2xl font-semibold '>UTOPIA STORE</p></div>
+            <p className='text-2xl font-semibold '>SENIOR SPHERE</p></div>
             <ul id="footerlinks" className='flex list-none gap-10 text-lg'>
                 <li className='cursor-pointer '>Company</li>
-                <li className='cursor-pointer '>Products</li>
                 <li className='cursor-pointer '>Offices</li>
                 <li className='cursor-pointer '>About</li>
                 <li className='cursor-pointer '>Contact</li>
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <nav className='flex justify-around p-5 w-screen h-24 shadow-md'>
         <div id="logo" className='flex items-center gap-1'>
         <img src="/Assets/logo.svg" alt="logo" className='w-24'></img>
-        <p className='text-2xl'>SeniorSphere</p></div>
+        <p className='text-2xl font-semibold'>SENIOR SPHERE</p></div>
         <div id="logincart" className='flex items-center gap-10'>
             <Link href="/Customer"><button className='w-44 h-14 outline-none border-gray-300 border  rounded-full text-lg cursor-pointer text-gray-700'>Customer Service</button></Link>
         </div>
