49cc7eae08aa73ce0cc93828344709bf5fa1d7f4
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2023-11-11 06:34:36 +0530

    updated home page

--- Frontend/app/page.js
+++ Frontend/app/page.js
@@ -7,10 +7,29 @@ import { withRouter } from 'next/router';
 export default function Home() {
   return(
     <>
-  <div>
-    Welcome to Elderly Care
-    <p>Please Sign in to enjoy all the fun.</p>
-  </div>
+    <div
+      style={{
+        backgroundImage: 'url("/Assets/homepage.png")',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+      className="min-h-screen bg-[image-name] bg-cover bg-no-repeat"
+    >
+      {/* Your content goes here */}
+      <div
+          style={{
+            position: 'absolute',
+            top: '70%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+            color: 'white',
+          }}
+        >
+          <h1 className="text-4xl font-extrabold">Senior Sphere</h1>
+          <p className="text-lg">Golden Years, Digital Cheers: Creating Joyful Connections for Seniors</p>
+        </div>
+    </div>
     </>
   )
 };
