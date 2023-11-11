89882f6dd373854e8fb6dddde3d590bdd5770637
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2023-11-11 09:57:14 +0530

    fixed bugs

--- Frontend/app/Social/page.jsx
+++ Frontend/app/Social/page.jsx
@@ -9,38 +9,22 @@ const page = () => {
     <div className=' flex grid-cols-main h-screen w-screen max-h-screen '>
       <div>
         <ChatListHeader />
-        <div className='chat section'>
-         <div className='h-16 bg-blue-100 pr-64 flex px-4 py-3 justify-between items-center chat'>
-            <div className='cursor-pointer'>
-                
-            </div>
-            <div className='flex gap-6'>
-
-            </div>
+        <div className='chat section flex flex-col'>
+         <div className='h-16 bg-blue-100  flex px-4 py-3 justify-between items-center chat gap-5'>
+         <h1 className=' '>Abhi</h1>
+                    <p className='text-gray-500 w-60'>hey Mom,How are you?</p>
          </div>
-         <div className='h-16 bg-blue-100 pr-64 flex px-4 py-3 justify-between items-center chat'>
-            <div className='cursor-pointer'>
-
-            </div>
-            <div className='flex gap-6'>
-
-            </div>
+         <div className='h-16 bg-blue-100 flex px-4 py-3 justify-between items-center chat'>
+         <h1 className=' '>Priya</h1>
+                    <p className='text-gray-500 w-60'>hey Aunt,Had food?</p>
          </div>
-         <div className='h-16 bg-blue-100 pr-64 flex px-4 py-3 justify-between items-center chat'>
-            <div className='cursor-pointer'>
-
-            </div>
-            <div className='flex gap-6'>
-
-            </div>
+         <div className='h-16 bg-blue-100  flex px-4 py-3 justify-between items-center chat'>
+         <h1 className=' '>Dr.Smit</h1>
+                    <p className='text-gray-500 w-60'>Mrs.Seema,Are you fine?</p>
          </div>
-         <div className='h-16 bg-blue-100 pr-64 flex px-4 py-3 justify-between items-center chat'>
-            <div className='cursor-pointer'>
-
-            </div>
-            <div className='flex gap-6'>
-
-            </div>
+         <div className='h-16 bg-blue-100   flex px-4 py-3 justify-between items-center chat flex-row flex-wrap'>
+                    <h1 className=' '>Abhi</h1>
+                    <p className='text-gray-500 w-60'>hey Mom,How are you?</p>
          </div>
 
 
--- Frontend/app/components/ChatListHeader.jsx
+++ Frontend/app/components/ChatListHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ChatListHeader = () => {
   return (
-    <div className='h-16 bg-slate-200 pr-64 flex px-4 py-3 justify-between items-center chat'>
+    <div className='h-16 bg-slate-200  flex px-4 py-3 justify-between items-center chat'>
          
          <div className='cursor-pointer'>
             <p>CONTACTS</p>
