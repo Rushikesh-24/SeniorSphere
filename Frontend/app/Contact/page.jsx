d93688d55e656266508552bae1f5efb04bbb110a
Rushikesh-24 <rushikeshgaokar24@gmail.com>
2023-11-11 06:19:56 +0530

    updated contact

--- Frontend/app/Contact/page.jsx
+++ Frontend/app/Contact/page.jsx
@@ -1,5 +1,4 @@
 "use client"
-// pages/emergency-contacts.js
 import React, { useState, useEffect } from 'react';
 
 const EmergencyContacts = () => {
@@ -12,20 +11,17 @@ const EmergencyContacts = () => {
   ];
 
   useEffect(() => {
-    // Fetch user-added emergency contacts from a database or storage
-    // For now, using local storage for simplicity
     const storedContacts = JSON.parse(localStorage.getItem('emergencyContacts')) || [];
     setEmergencyContacts(storedContacts);
   }, []);
 
   useEffect(() => {
-    // Update local storage when emergencyContacts change
     localStorage.setItem('emergencyContacts', JSON.stringify(emergencyContacts));
   }, [emergencyContacts]);
 
   const addContact = () => {
     if (newName.trim() !== '' && newContact.trim() !== '') {
-      const newEmergencyContact = `${newName}: ${newContact}`;
+      const newEmergencyContact = { name: newName, number: newContact };
       setEmergencyContacts([...emergencyContacts, newEmergencyContact]);
       setNewName('');
       setNewContact('');
@@ -42,37 +38,61 @@ const EmergencyContacts = () => {
     <div className='flex justify-center flex-col items-center'>
       <h1 className='font-semibold text-4xl mt-3'>Emergency Contacts</h1>
       <h2 className='mt-8 text-gray-700 text-2xl'>Fixed Numbers</h2>
-      <ul>
-        {fixedNumbers.map(({ name, number }, index) => (
-          <li key={index} className='mt-4 flex  items-center justify-center'>
-            {name}: {number}
-          </li>
-        ))}
-      </ul>
+      <table className='mt-4 border-collapse border'>
+        <thead>
+          <tr>
+            <th className='border p-4 px-10'>Name</th>
+            <th className='border p-5 px-10'>Contact Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fixedNumbers.map(({ name, number }, index) => (
+            <tr key={index}>
+              <td className='border p-2'>{name}</td>
+              <td className='border p-2'>{number}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <h2 className='mt-8 text-gray-700 text-2xl'>User-added Numbers</h2>
-      <ul className='flex items-center justify-center'>
-        {emergencyContacts.map((contact, index) => (
-          <li key={index} className='mt-4 flex  items-center justify-center'>
-            {contact}
-            <button onClick={() => removeContact(index)} className='rounded-2xl bg-red-500 w-40 h-12 mx-4' >Remove</button>
-          </li>
-        ))}
-      </ul>
+      <table className='mt-4 border-collapse border'>
+        <thead>
+          <tr>
+            <th className='border p-2 px-10'>Name</th>
+            <th className='border p-2 px-10'>Contact Number</th>
+            <th className='border p-2'>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {emergencyContacts.map((contact, index) => (
+            <tr key={index}>
+              <td className='border p-2'>{contact.name}</td>
+              <td className='border p-2'>{contact.number}</td>
+              <td className='border p-2'>
+                <button onClick={() => removeContact(index)} className='rounded-2xl bg-red-500 text-white px-4 py-2'>Remove</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <div className='gap-3 flex justify-center items-center mt-5'>
         <input
           type="text"
           value={newName}
           onChange={(e) => setNewName(e.target.value)}
           placeholder="Enter a name"
-          className='h-10'
+          className='h-12 w-56 border-2 shadow-xl'
         />
         <input
           type="text"
           value={newContact}
           onChange={(e) => setNewContact(e.target.value)}
           placeholder="Enter a contact number"
+          className='h-12 border-2 shadow-xl w-56'
         />
-        <button onClick={addContact} className='rounded-2xl bg-green-500 w-40 h-12 '>Add Contact</button>
+        <button onClick={addContact} className='rounded-2xl bg-green-500 text-white px-4 py-2'>Add Contact</button>
       </div>
     </div>
   );
